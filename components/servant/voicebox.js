4ad8604cdd0c7e678929ac3a48cfa82926c1e0bc
grape <leah.ren@hotmail.com>
2021-10-17 03:15:22 -0600

    fixed tooltip on voicelines

--- components/servant/voicebox.js
+++ components/servant/voicebox.js
@@ -1,14 +1,17 @@
 import styles from './servant.module.css'
 import SpecialText from './specialtext';
+import DialogText from './dialogtext';
 import Typed from 'react-typed';
+import ReactDOMServer from 'react-dom/server'
 
 const Voicebox = ({colour,data}) => {
+    var voicetext = <DialogText data={data}/>;
     return(<p style={{
         display: "flex",
         backgroundColor: colour,
       }} className={styles.voiceLine}>{[<i className={"bi bi-play-circle-fill" + ' ' + styles.playIcon} ></i>,
                                         <Typed
-                    strings={[data]}
+                    strings={[ReactDOMServer.renderToString(voicetext)]}
                     typeSpeed={8}
                     showCursor={false}
                 />]}</p>);
