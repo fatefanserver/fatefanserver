6d85498b85580aebe9ac62fd5757975e78d11ac7
grape <leah.ren@hotmail.com>
2021-10-18 02:25:25 -0600

    added play button for voice lines

--- components/servant/specialtext.js
+++ components/servant/specialtext.js
@@ -22,12 +22,8 @@ const CustomRenderers = {
                                     return <span style={{color: furigana[(i-1)/2][3]}}>{v}</span>
                                 case 'tp':
                                     // tooltip
-                                    if(mode == 'scroll'){
-                                        return <div className={styles.tpd}>{v}<span className={styles.tooltiptext}>{furigana[(i-1)/2][3]}</span></div>
-                                    }
-                                    else{
                                         return <div className={styles[tag]}>{v}<span className={styles.tooltiptext}>{furigana[(i-1)/2][3]}</span></div>
-                                    }
+                                    
                                     
                                 default:
                                     return v;
@@ -44,9 +40,44 @@ const CustomRenderers = {
     }
 }
 
+const CustomRenderersScroll = {
+    p: ({children, mode}) => {
+        const checkregexTags = /#\w/;
+        // checks for a pattern of [text]{#tag tag text} and returns them in capture groups.
+        const regexFg = /\[([\S ][^\]]+)\]\{#(\w+) ([\S ][^\}]+)\}/g;
+        for(var i = 0; i < children.length; i++){
+            if(typeof(children[i]) === 'string'){
+                if(checkregexTags.test(children[i])){
+                    const furigana = [...children[i].matchAll(regexFg)];
+                    children[i] = children[i].split(/\[([\S ][^\]]+)\]\{#\w+ [\S ][^\}]+\}/g).map((v,i) => {
+                        if(i % 2){
+                            var tag = furigana[(i-1)/2][2];
+                            switch(tag){
+                                case 'fg':
+                                    // furigana
+                                    return <span className={styles[tag]} t={furigana[(i-1)/2][3]}>{v}</span>
+                                case 'c':
+                                    // colour text
+                                    return <span style={{color: furigana[(i-1)/2][3]}}>{v}</span>
+                                case 'tp':
+                                    // tooltip
+                                    return <div className={styles.tpd}>{v}<span className={styles.tooltiptext}>{'`'+ furigana[(i-1)/2][3] + '`'}</span></div>
+                                default:
+                                    return v;
+                            }
+                        }
+                        else{
+                            return v
+                        }
+                    });
+                }
+            }
+        }
+        return children
+    }
+}
+
 const SpecialText = ({data, mode}) => {
-    return(
-        <ReactMarkdown components={CustomRenderers}>{data}</ReactMarkdown>
-    )
+    return(<ReactMarkdown components={ mode == "scroll"? CustomRenderersScroll : CustomRenderers}>{data}</ReactMarkdown>)
 };
 export default SpecialText;
--- components/servant/voicebox.js
+++ components/servant/voicebox.js
@@ -1,22 +1,83 @@
 import styles from './servant.module.css'
-import DialogText from './dialogtext';
 import SpecialText from './specialtext';
-import Typed from 'react-typed';
+import Typed from 'typed.js';
 import ReactDOMServer from 'react-dom/server'
+import { Component } from 'react';
 
-const Voicebox = ({colour,data}) => {
-    var voicetext = <SpecialText data={data}/>;
+class Voicebox extends Component{
+    state = {
+        buttonState: "play",
+        isDone: true,
+        minHeight: 50,
+        // PUT MAX HEIGHT HERE
+        maxHeight: 100,
+        firstLoop: true,
+    }
+    componentDidMount() {
+        var voicetext = <SpecialText data={this.props.data} mode={"scroll"}/>;
+        var getvoicestring = ReactDOMServer.renderToString(voicetext);
+        //function setStateFunction () {this.state.isDone = true};
+        const options = {
+            strings: [getvoicestring],
+            typeSpeed: 8,
+            showCursor: false,
+            onComplete: () =>this.onDone(),
+        };
+        this.typed = new Typed(this.el, options);
+        this.typed.destroy();
+    }
+    componentWillUnmount() {
+        this.typed.destroy();
+    }
+    toggleIcon = () => {
+        this.setState(state => ({ buttonState: this.state.buttonState == "play" ? "pause" : "play"}));
+    }
+    onDone = () => {
+        
+        this.setState(state => ({ isDone: true}));
+        this.toggleIcon();
+    }
+    pauseIcon = () => {
+        this.typed.toggle();
+        this.toggleIcon();
+    }
+    resetIcon = () => {
+        if(this.state.firstLoop){
+            var curheight = this.el.parentElement.parentElement.getBoundingClientRect().height;
+            this.setState(state => ({ firstLoop: false, isDone: false, minHeight: curheight}));
+            this.typed.reset();
+            this.toggleIcon();
+        }
+        else{
+            this.setState(state => ({ isDone: false}));
+            this.typed.reset();
+            this.toggleIcon();
+        }
+    }
+    stopTyping = () => {
+        this.typed.destroy();
+    }
+    getButton = () => this.state.buttonState;
+    getDone = () => this.state.isDone;
+    getLoop = () => this.state.firstLoop;
+    render(){
+    const curButton = this.getButton();
+    const stateDone = this.getDone();
+    const loopState = this.getLoop();
     return(<p style={{
         display: "flex",
-        backgroundColor: colour,
+        backgroundColor: this.props.colour,
         maxHeight: "100px",
+        minHeight: this.state.minHeight,
         position: "relative",
-      }} className={styles.voiceLine}>{[<i key="icon" className={"bi bi-play-circle-fill" + ' ' + styles.playIcon} ></i>,
-                                        <i className={styles.voiceText}><Typed key="para"
-                    strings={[ReactDOMServer.renderToString(voicetext)]}
-                    typeSpeed={8}
-                    showCursor={false}
-                /></i>]}</p>);
-};
+      }} className={styles.voiceLine}>{[<i key="icon" onClick={() => stateDone ? this.resetIcon() : this.pauseIcon()} 
+                                        className={"bi bi-"+curButton+"-circle-fill" + ' ' + styles.playIcon}/>,
+                <span style={{paddingRight:"20px"}}></span>,
+                <i className={styles.voiceText}><span ref={(el) => { this.el = el; }} key="para"/>{
+                    <SpecialText data={loopState ? this.props.data : ''} key="para2" mode={"scroll"}/>
+                }</i>]}
+            </p>);
+    }
+}
 
 export default Voicebox;
